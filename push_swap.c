739483372ddd967a84d68a95f0560d52cd95a0b9
mink97 <kmg8146@naver.com>
2022-12-12 20:22:10 +0900

    add free_deq()

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,26 @@
 /*   By: mingkang <mingkang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:39:45 by mingkang          #+#    #+#             */
-/*   Updated: 2022/12/12 13:30:18 by mingkang         ###   ########.fr       */
+/*   Updated: 2022/12/12 20:21:46 by mingkang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	free_deq(t_deque *deq)
+{
+	t_node	*tmp;
+	t_node	*dummy;
+
+	tmp = deq->head;
+	while (tmp != NULL)
+	{
+		dummy = tmp;
+		tmp = tmp->next;
+		free(dummy);
+	}
+	free(deq);
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,4 +37,7 @@ int	main(int argc, char **argv)
 	init_deque(argc - 1, argv + 1, &deq_a, &deq_b);
 	handover_a_to_b(deq_a, deq_b);
 	handover_b_to_a(deq_b, deq_a);
+	free_deq(deq_a);
+	free_deq(deq_b);
+	return (0);
 }
